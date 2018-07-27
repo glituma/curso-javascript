cd8d66cf613f46e2769664b9551534feb5680c1d
glituma <glituma@gmail.com>
2018-07-27 09:57:15 -0500

    commit

--- hola-mundo/js/22-ambito.js
+++ hola-mundo/js/22-ambito.js
@@ -1,13 +1,20 @@
 'use strict'
 
-//Variables de Ambito
+//Variables de Ámbito
 
 
 
 function holaMundo(texto){
+    var hola_mundo = "Texto dentro de función: ";
     console.log(texto);
+    console.log(numero.toString());
+    console.log(hola_mundo);
 }
+
+var numero = 12
 var texto = "";
 texto = "soy una variable global: ";
 
 holaMundo(texto);
+
+//console.log(hola_mundo);
