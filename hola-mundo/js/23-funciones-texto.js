da0afd0e088bd5046bf9457972a3dadd3e7c5d20
glituma <glituma@gmail.com>
2018-09-10 17:10:52 -0500

    Subido 10-09-2018

--- hola-mundo/js/23-funciones-texto.js
+++ hola-mundo/js/23-funciones-texto.js
@@ -3,7 +3,7 @@
 //Transformar textos
 var numero= 444;
 var texto1 = "Bienvenido al Curso de JavaScript";
-var texto2 = "muy buen curso";
+var texto2 = "muy buen Curso";
 
 var dato = numero.toString();
     console.log(dato);
@@ -28,4 +28,32 @@ console.log(textoTotal);
 var busqueda = texto1.indexOf("al");
 console.log(busqueda);
 var busqueda = textoTotal.lastIndexOf("so");
+console.log(busqueda);
+
+busqueda = textoTotal.search("Curso");
+console.log(busqueda);
+
+busqueda = textoTotal.match("Curso");
+console.log(busqueda);
+
+busqueda = textoTotal.match(/Curso/g);
+console.log(busqueda);
+
+busqueda = textoTotal.substr(24,5);
+console.log(busqueda);
+
+busqueda = textoTotal.charAt(34);
+console.log(busqueda);
+
+busqueda = textoTotal.startsWith("Bienvenido");
+console.log(busqueda);
+
+busqueda = textoTotal.endsWith("Curso");
+console.log(busqueda);
+
+busqueda = textoTotal.includes("JavaScript");
+console.log(busqueda);
+
+//Remplazar texto
+busqueda = textoTotal.replace("JavaScript",".net 2010");
 console.log(busqueda);
